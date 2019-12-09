e6ea06ecac2ba48ce990977b14459908c7c1c519
Luca Benedetti <luca.benedetti@euei.it>
2019-12-09 14:05:56 +0100

    Starting commit.

---
+++ js/index.js
@@ -0,0 +1,18 @@
+document.getElementById('opening').addEventListener('click', onClick, false);
+function onClick() {
+  if (
+    document.getElementsByClassName('top-arrow')[0].classList.contains('rotate')
+  ) {
+    document.getElementsByClassName('top-arrow')[0].classList.remove('rotate');
+    document.getElementsByClassName('bot-arrow')[0].classList.remove('rotate');
+    document.getElementById('top-cover').classList.remove('final');
+    document.getElementsByClassName('span-opening')[0].classList.remove('black');
+    document.getElementsByTagName('body')[0].classList.remove('overflow');
+  } else {
+    document.getElementsByClassName('top-arrow')[0].classList.add('rotate');
+    document.getElementsByClassName('bot-arrow')[0].classList.add('rotate');
+    document.getElementById('top-cover').classList.add('final');
+    document.getElementsByClassName('span-opening')[0].classList.add('black');
+    document.getElementsByTagName('body')[0].classList.add('overflow');
+  }
+}
