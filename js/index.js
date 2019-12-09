cd89a89bd2a85383910264651a8710576a2560f9
Luca Benedetti <luca.benedetti@euei.it>
2019-12-09 15:18:38 +0100

    Revert wrapper addition.

--- js/index.js
+++ js/index.js
@@ -7,12 +7,12 @@ function onClick() {
     document.getElementsByClassName('bot-arrow')[0].classList.remove('rotate');
     document.getElementById('top-cover').classList.remove('final');
     document.getElementsByClassName('span-opening')[0].classList.remove('black');
-    document.getElementById('wrapper').classList.remove('overflow');
+    document.getElementsByTagName('body')[0].classList.remove('overflow');
   } else {
     document.getElementsByClassName('top-arrow')[0].classList.add('rotate');
     document.getElementsByClassName('bot-arrow')[0].classList.add('rotate');
     document.getElementById('top-cover').classList.add('final');
     document.getElementsByClassName('span-opening')[0].classList.add('black');
-    document.getElementById('wrapper').classList.add('overflow');
+    document.getElementsByTagName('body')[0].classList.add('overflow');
   }
 }
